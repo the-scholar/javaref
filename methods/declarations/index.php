71aad501bfd87a366551e08692fc8ecd10931be6
the-scholar <the-scholar@dusttoash.org>
2023-04-12 21:55:42 -0500

    Update page on Method Declarations

--- methods/declarations/index.php
+++ methods/declarations/index.php
@@ -13,10 +13,32 @@
 		<td>1</td>
 		<td><span class="syntax-piece">modifier-list</span> <span
 			class="syntax-piece">return-type</span> <span class="syntax-piece">name</span>
-			<code>(</code><span class="syntax-piece">parameter-list</span><code>)</code>
+			<code>(</code><span class="syntax-piece optional">parameter-list</span><code>)</code>
 			<span class="syntax-piece optional">array-dims</span> <span
 			class="optional"><code>throws</code> <span class="syntax-piece">throw-list</span></span>
 			<span class="syntax-piece">block-body</span></td>
 	</tr>
+	<tr>
+		<td>2</td>
+		<td><span class="syntax-piece">modifier-list</span> <code>&lt;</code>
+			<span class="syntax-piece">type-parameter-list</span> <code>&gt;</code>
+			<span class="syntax-piece">annotation-list</span> <span
+			class="syntax-piece">name</span> <code>(</code> <span
+			class="syntax-piece optional">parameter-list</span> <code>)</code> <span
+			class="optional syntax-piece">array-dims</span> <span
+			class="optional"><code>throws</code> <span class="syntax-piece">throw-list</span></span>
+			<span class="syntax-piece">block-body</span></td>
+	</tr>
 </table>
+<p>
+	For a more human-friendly breakdown, see <a
+		href="#AppendixA">Appendix A</a>.
+</p>
+
+<section sect-symbol="A" id="AppendixA">
+	<h1>Appendix A</h1>
+	
+</section>
+
+
 <?php b();?>
