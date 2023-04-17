68b650d5ecee89cd9deb39d8e4835e1d374f9823
the-scholar <the-scholar@dusttoash.org>
2023-04-16 21:04:22 -0500

    Create Boolean Literal page

---
+++ literals/boolean/index.php
@@ -0,0 +1,18 @@
+<?php t("Javaref - Boolean Literals");?>
+<h1>Boolean Literals</h1>
+<p class="description">
+	Literal expression of a boolean (<code>true</code>/<code>false</code>)
+	value in source code.
+</p>
+<h2>Syntax</h2>
+<p>Boolean literals have two forms:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><code>true</code></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td><code>false</code></td>
+	</tr>
+</table>
