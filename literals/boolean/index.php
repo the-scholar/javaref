4ee1d9d089f89f8c936255afe9a12d83d0d56c76
the-scholar <the-scholar@dusttoash.org>
2023-04-17 02:49:33 -0500

    Update Boolean Literal page

--- literals/boolean/index.php
+++ literals/boolean/index.php
@@ -1,7 +1,7 @@
 <?php t("Javaref - Boolean Literals");?>
 <h1>Boolean Literals</h1>
 <p class="description">
-	Literal expression of a boolean (<code>true</code>/<code>false</code>)
+	Literal expression of a Boolean (<code>true</code>/<code>false</code>)
 	value in source code.
 </p>
 <h2>Syntax</h2>
@@ -22,4 +22,10 @@
 </p>
 <p>
 	<span class="syntax-number">2</span> The literal for the value <code>false</code>.
+</p>
+<h2>Usage</h2>
+<p>
+	The type of a Boolean literal is always the <code>boolean</code> type.
+	Boolean literals can be used in any context that a boolean expression
+	is acceptable.
 </p>
