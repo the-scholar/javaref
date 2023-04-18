e4c6dee0a35d65a9725f05319379ed0e9fc74665
the-scholar <the-scholar@dusttoash.org>
2023-04-17 22:42:25 -0500

    Update page on Identifiers

--- identifiers/index.php
+++ identifiers/index.php
@@ -20,15 +20,15 @@
 <table class="syntax-breakdown">
 	<tr>
 		<td><span class="syntax-piece">ident-start-char</span></td>
-		<td>is either <code>_</code>, <code>$</code>, or any valid unicode
-			letter.
+		<td>is either <code>_</code>, <code>$</code>, or any valid letter (<a
+			href="">see below</a>).
 		</td>
 	</tr>
 	<tr>
 		<td><span class="syntax-piece">ident-body-chars</span></td>
 		<td>is a sequence of characters composed entirely of: either <code>_</code>,
-			<code>$</code>, or any valid unicode letter or digit. The sequence
-			may be of any size.
+			<code>$</code>, or any valid letter or digit (<a href="">see below</a>).
+			The sequence may be of any size.
 		</td>
 	</tr>
 </table>
@@ -45,3 +45,31 @@
 	<span class="syntax-number">1</span> An identifier, used to name a
 	variable, method, class or other type, type parameter, etc.
 </p>
+<h2>Composition</h2>
+<p>
+	Java identifiers are composed of Unicode characters, defined by the
+	Unicode standard. The Unicode characters that can comprise an
+	identifier are defined by the static Java API methods <code>Character.isJavaIdentifierStart(int)</code>
+	and <code>Character.isJavaIdentifierPart(int)</code>, for the <span
+		class="syntax-piece">ident-start-char</span> and each character in <span
+		class="syntax-piece">ident-body-chars</span>, respectively; if either
+	method returns <code>true</code> for a character, then the respective
+	syntax element can be composed of that character in a valid identifier.
+</p>
+<p>
+	Unicode groups characters into <i>blocks</i> and into <i>general
+		categories</i> (and sub-categories).
+<p>The first character of an identifier is any one of the following:</p>
+<ul>
+	<li>
+
+</ul>
+<h2>References</h2>
+<ol>
+	<li
+		title="The Unicode Standard, Version 15.0.0, Chapter 4: General Category"><a
+		href="https://www.unicode.org/versions/Unicode15.0.0/ch04.pdf#page=15">Unicode
+			Standard: General Category</a> - Precise description of <i>General
+			Category</i> defined by the Unicode Standard. (The link leads to
+		version 15.0.0 of the standard.)</li>
+</ol>
