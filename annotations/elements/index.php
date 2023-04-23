aeeb01c38fb778d5c68b198756d830226d98cb27
the-scholar <the-scholar@dusttoash.org>
2023-04-23 06:24:55 -0500

    Update Annotation Element Declarations page

--- annotations/elements/index.php
+++ annotations/elements/index.php
@@ -1,1 +1,11 @@
-<?php t("Annotation Elements", "Used to specify the properties that need to be given values when an annotation is used.");?>
+<?php t("Annotation Element Declarations", "Used to specify the properties that need to be given values when an annotation is used.");?>
+<h1>Annotation Element Declarations</h1>
+<p class="description">Defines a property of an annotation.</p>
+<p>
+	Annotations may, optionally, specify <i>elements</i> in their body,
+	which define properties that are provided in each use of the
+	annotation. The elements may have a default value, in which case their
+	being specified in an annotation's use is optional.
+</p>
+<h2>Syntax</h2>
+<p>An annotation element declaration consists of:</p>
