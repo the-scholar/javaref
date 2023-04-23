5c1d44b795d0487c3ccf4301dd20f27111c56be4
the-scholar <the-scholar@dusttoash.org>
2023-04-23 00:52:52 -0500

    Create Annotation Declarations page

---
+++ annotations/declarations/index.php
@@ -0,0 +1,15 @@
+<?php t("Annotation Declarations", "Introduces an annotation type to the program that can be used to annotate elements.");?>
+<h1>Annotation Declarations</h1>
+<p class="description">Introduces an annotation type to the program,
+	which can be used to annotate targeted elements.</p>
+<h2>Syntax</h2>
+<p>An annotation declaration consists of:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><span class="syntax-piece optional">modifier-list</span> <code>@</code>
+			<code>interface</code> <span class="syntax-piece">name</span> <code>{</code>
+			<span class="syntax-piece optional">annotation-members</span> <code>}</code>
+		</td>
+	</tr>
+</table>
