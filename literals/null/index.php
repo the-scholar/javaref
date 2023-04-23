0b1472d72aa4582a53dfd28ea98630672cfab527
the-scholar <the-scholar@dusttoash.org>
2023-04-23 00:47:26 -0500

    Create null literal page

---
+++ literals/null/index.php
@@ -0,0 +1,11 @@
+<?php t("Javaref - Null Literal", "The null value expresses the special null value in source code, and can be assigned to any reference type.");?>
+<h1>Null Literal</h1>
+<p class="description">
+	Literal expression of the <code>null</code> value.
+</p>
+<p>
+	The <code>null</code> literal is used to denote the special null value,
+	which cast to any reference type. There is only one null value whose
+	type is the null type. The null type cannot be expressed in Java code.
+</p>
+<h2>Syntax</h2>
