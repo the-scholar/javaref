0b7b385cf0a2dda077b9fdf578dd3ac314ebb2eb
the-scholar <the-scholar@dusttoash.org>
2023-04-23 18:36:29 -0500

    Update Annotation Element Declarations page

--- annotations/elements/index.php
+++ annotations/elements/index.php
@@ -9,3 +9,59 @@
 </p>
 <h2>Syntax</h2>
 <p>An annotation element declaration consists of:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><span class="syntax-piece optional">modifier-list</span> <span
+			class="syntax-piece">type</span> <span class="syntax-piece">name</span>
+			<code>(</code> <code>)</code> <span class="syntax-piece optional">array-dims</span>
+			<span class="optional"><code>default</code> <span
+				class="syntax-piece">default-value</span></span> <code>;</code></td>
+	</tr>
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">modifier-list</span></td>
+		<td>is a possibly empty set of keywords and annotations that can
+			contain, <code>public</code>, <code>abstract</code>, and any number
+			of annotations that are applicable to the annotation element.
+		</td>
+	</tr>
+	<tr>
+		<td><span class="syntax-piece">type</span></td>
+		<td>is one of the following:
+			<ul>
+				<li>A primitive type,</li>
+				<li>the type, <code>Class</code> (possibly parameterized),
+				</li>
+				<li><code>String</code>,</li>
+				<li>any <code>enum</code> type,
+				</li>
+				<li>any other annotation type that does not have an element of this
+					annotation type (directly or indirectly), or</li>
+				<li>an array of any of these types of max dimensionality <code>1</code>.
+				</li>
+			</ul>
+		</td>
+	</tr>
+	<tr>
+		<td><span class="syntax-piece">name</span></td>
+		<td>is an identifier denoting the name of the element.</td>
+	</tr>
+	<tr>
+		<td><span class="syntax-piece">array-dims</span></td>
+		<td>is the <code>[</code> token followed by the <code>]</code> token,
+			used together to indicate that the element's type is an array. The
+			brackets can instead be included in the <span class="syntax-piece">type</span>,
+			but are allowed here for consistency with older versions.
+		</td>
+	</tr>
+	<tr>
+		<td><span class="syntax-piece">default-value</span></td>
+		<td>is a constant expression, whose type is the same as the element's
+			declared type.</td>
+	</tr>
+</table>
