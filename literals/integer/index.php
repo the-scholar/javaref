901866e0ea84b20b0d0d189e3c5d9ff5e6d2e826
the-scholar <the-scholar@dusttoash.org>
2023-04-23 19:01:50 -0500

    Fix spacing in syntax table for Int Literals

--- literals/integer/index.php
+++ literals/integer/index.php
@@ -12,22 +12,22 @@
 <table class="syntax">
 	<tr>
 		<td>1</td>
-		<td><span class="syntax-piece">decimal-literal</span> <span
+		<td><span class="syntax-piece">decimal-literal</span><span
 			class="syntax-piece optional">integer-type-suffix</span></td>
 	</tr>
 	<tr>
 		<td>2</td>
-		<td><span class="syntax-piece">hexadecimal-literal</span> <span
+		<td><span class="syntax-piece">hexadecimal-literal</span><span
 			class="syntax-piece optional">integer-type-suffix</span></td>
 	</tr>
 	<tr>
 		<td>3</td>
-		<td><span class="syntax-piece">octal-literal</span> <span
+		<td><span class="syntax-piece">octal-literal</span><span
 			class="syntax-piece optional">integer-type-suffix</span></td>
 	</tr>
 	<tr>
 		<td>4</td>
-		<td><span class="syntax-piece">binary-literal</span> <span
+		<td><span class="syntax-piece">binary-literal</span><span
 			class="syntax-piece optional">integer-type-suffix</span></td>
 	</tr>
 </table>
