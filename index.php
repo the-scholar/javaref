37a595b72c0ffefb36ecca7876a051200617bf08
the-scholar <the-scholar@dusttoash.org>
2023-05-09 02:09:32 -0500

    Update homepage

--- index.php
+++ index.php
@@ -5,36 +5,14 @@
 <link rel="stylesheet" type="text/css" href="/index.css">
 <title>Javaref.net</title>
 <style type="text/css">
-#ToC {
-	font-size: .8em;
-	line-height: 90%;
-	color: lightgray;
-	columns: 4;
-}
-
-#ToC>div {
-	display: block;
-}
-
-#ToC a, #ToC span {
-	font-weight: bold;
+#ToC .sec {
 	font-variant: small-caps;
-	display: inline-block;
-	padding-bottom: .4em;
+	font-weight: bold;
+	font-size: 1.1em;
 }
 
-#ToC a.c {
-	font-size: .75em;
-	padding-left: 1.5em;
-	font-weight: normal;
-	opacity: .6;
-	font-variant: normal;
-	padding-bottom: 0;
-	display: inline;
-}
-
-#ToC a.c:hover {
-	opacity: 1;
+#ToC a {
+	font-size: calc(1em/ 1.1);
 }
 
 a.u {
@@ -51,49 +29,44 @@ a.u {
 	</div>
 	<div id="Content">
 		<h1>Java Reference</h1>
+		<p>This is a list of all existing pages on this site:</p>
 		<p>Table of contents:</p>
-		<ul>
-			<li>General
+		<ul id="ToC">
+			<li><span class="sec">General</span>
 				<ul>
 					<li><a class="c" href="/identifiers">Identifiers</a></li>
 					<li><a class="c" href="/initializers">Static &amp; Instance
 							Initializers</a></li>
-				</ul>
-			</li>
-			<li>Literals
+				</ul></li>
+			<li><span class="sec">Literals</span>
 				<ul>
 					<li><a class="c" href="/literals/integer">Integer Literal</a></li>
 					<li><a class="c" href="/literals/floating-point">Floating Point
 							Literal</a></li>
 					<li><a class="c" href="/literals/boolean">Boolean Literal</a></li>
 					<li><a class="c u" href="/literals/null">Null Literal</a></li>
-				</ul>
-			</li>
-			<li>Methods
+				</ul></li>
+			<li><span class="sec">Methods</span>
 				<ul>
 					<li><a class="c" href="/methods/declarations">Method Declarations</a></li>
 					<li><a class="c" href="/methods/receiver-parameters">Receiver
 							Parameters</a></li>
-				</ul>
-			</li>
-			<li>Operators
+				</ul></li>
+			<li><span class="sec">Operators</span>
 				<ul>
-					<li>Unary
+					<li><span class="sec">Unary</span>
 						<ul>
 							<li><a class="c u" href="/operators/unary/cast">Cast Operator</a></li>
-						</ul>
-					</li>
-				</ul>
-			</li>
-			<li>Annotations
+						</ul></li>
+				</ul></li>
+			<li><span class="sec">Annotations</span>
 				<ul>
 					<li><a class="c u" href="annotations">Annotations</a></li>
 					<li><a class="c u" href="annotations/declarations">Annotation
 							Declarations</a></li>
 					<li><a class="c" href="annotations/elements">Annotation Element
 							Declarations</a></li>
-				</ul>
-			</li>
+				</ul></li>
 		</ul>
 		<h3>Notes</h3>
 		<p>
