76cc2d6bf9cafd3851e75b430b9b39c52d3c06e3
the-scholar <the-scholar@dusttoash.org>
2023-05-09 01:50:26 -0500

    Create Cast Operator page

---
+++ operators/unary/cast/index.php
@@ -0,0 +1,59 @@
+<?php t("Javaref - Cast Operators", "The Java cast operator attempts to coerce the type of an expression, throwing a ClassCastException upon failure.");?>
+<h1>Cast Operator</h1>
+<p class="description">Changes the type of an expression without
+	changing the value.</p>
+<p>
+	The cast operator changes or specifies the type of an expression,
+	throwing a <code>ClassCastException</code> if its argument cannot be
+	coerced to the specified type.
+</p>
+<h2>Syntax</h2>
+<p>Cast Operators can be expressed through:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><code>(</code> <span class="syntax-piece">type</span> <code>)</code>
+			<span class="syntax-piece">castable-expression</span></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td><code>(</code> <span class="syntax-piece">main-type</span> <span
+			class="optional syntax-piece">additional-types</span></td>
+	</tr>
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">type</span></td>
+		<td>is any type, except <code>void</code>. <!-- TODO: Check spec to clarify this. -->
+		</td>
+	</tr>
+</table>
+<p>
+	<i>such that...</i>
+</p>
+<ul>
+	<li>
+		<!-- TODO: Insert Such that clause -->
+	</li>
+</ul>
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> syntax_fragment_1_breakdown
+</p>
+<!-- TODO: Insert any additional syntax fragment breakdowns -->
+<h2>main_section_title</h2>
+<!-- TODO: Fill out main section -->
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li>
+		<!-- TODO: Add notes -->
+	</li>
+</ol>
+<?php
+
+b();
