bc3e17541096119c21d02133c4f9f8bca6473c2c
the-scholar <the-scholar@dusttoash.org>
2023-05-09 10:35:26 -0500

    Create Expressions page

---
+++ expressions/index.php
@@ -0,0 +1,53 @@
+<?php t("Javaref - Expressions", "A Java expression represents a sequence of operations carried out to arrive at a value. Every Java expression in source code has an associated type and, upon being computed at runtime, results in a value of that type.");?>
+<h1>Expression</h1>
+<p class="description">Represents a sequence of computations to arrive
+	at a value.</p>
+<p>Expressions specify the steps to compute a certain value. Each
+	expression has an associated type, which is known at compile time and
+	is used to minimally verify the validity of certain computations.</p>
+<!-- TODO: Decide on remainder of page. -->
+<h2>Syntax</h2>
+<p>Expressions can be expressed through:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td>syntax_fragment_1</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax fragments -->
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">syntax_piece_1</span></td>
+		<td>is syntax_piece_1_breakdown</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax piece breakdowns -->
+</table>
+<p>
+	<i>such that...</i>
+</p>
+<ul>
+	<li>
+		<!-- TODO: Insert Such that clause -->
+	</li>
+</ul>
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> syntax_fragment_1_breakdown
+</p>
+<!-- TODO: Insert any additional syntax fragment breakdowns -->
+<h2>main_section_title</h2>
+<!-- TODO: Fill out main section -->
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li>
+		<!-- TODO: Add notes -->
+	</li>
+</ol>
+<?php
+
+b();
