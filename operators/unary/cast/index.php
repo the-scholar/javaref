ebd2aa7d5af69d671f5d2e2d3f859ec11910fee4
the-scholar <the-scholar@dusttoash.org>
2023-05-12 00:25:53 -0500

    Update cast operator page

--- operators/unary/cast/index.php
+++ operators/unary/cast/index.php
@@ -39,23 +39,34 @@
 	</tr>
 	<tr>
 		<td><span class="syntax-piece">primitive-castable-expression</span></td>
-		<td>is any one of the following expressions:
+		<td>is an expression of one of the following types:
 			<ul>
 				<li>the application of any one unary operator (including another
 					cast),</li>
-				<li>any type of array creation expression,</li>
-				<li>a method reference,</li>
-				<li>the keyword <code>this</code> (possibly qualified by a type),
-				</li>
-				<li>a class literal,</li>
 				<li>a literal,</li>
 				<li>a parenthesized expression,</li>
-				<li>a class instance creation expression,</li>
-				<li>a reference to a field,</li>
+				<li>a reference to a field or variable,</li>
 				<li>an array component,</li>
 				<li>a method invocation,</li>
-				<li>an expression name.</li>
 			</ul>
+			<p>
+				Other types are syntactically, but not semantically<sup info=1></sup>
+				permitted.
+			</p> <span info=1>Syntactically, a <span class="syntax-piece">primitive-castable-expression</span>
+				can also be any one of the following expressions, but a
+				primitive-type&ndash;cast may only be applied to a type convertible
+				to a primitive type, and none of the following types can be
+				converted to a primitive type through a single cast.<!-- TODO -->
+				<ul>
+					<li>any type of array creation expression,</li>
+					<li>a method reference,</li>
+					<li>the keyword <code>this</code> (possibly qualified by a type),
+					</li>
+					<li>a class literal,</li>
+					<li>a class instance creation expression,</li>
+					<li>an expression name.</li>
+				</ul>
+		</span>
 		</td>
 	</tr>
 	<tr>
@@ -68,45 +79,41 @@
 	</tr>
 	<tr>
 		<td><span class="syntax-piece">reference-castable-expression</span></td>
-		<td>is the same<sup info=1></sup> as <span class="syntax-piece">primitive-castable-expression</span>,
-			except that <span class="syntax-piece">reference-castable-expression</span>
-			includes
+		<td>is an expression of one of the following types:
 			<ul>
 				<li>lambda expressions</li>
-			</ul>
-			<p>and excludes expressions that are an application of</p>
-			<ul>
-				<li>the pre-increment operator,</li>
-				<li>the pre-decrement operator,</li>
-				<li>the unary plus operator, and</li>
-				<li>the unary minus operator.</li>
-			</ul>
-			<span info=1>More formally, <span class="syntax-piece">reference-castable-expression</span>
-				is any of the following:
+				<li>only<sup info=2></sup> the application of any one of the
+					following operators:
+					<ul>
+						<li>the cast operator,</li>
+						<li>the logical negation operator,</li>
+						<li>the bitwise negation operator,</li>
+						<li>the post-increment operator,</li>
+						<li>the post-decrement operator,</li>
+					</ul>
+				</li>
+				<li>any type of array creation expression,</li>
+				<li>a method reference,</li>
+				<li>the keyword <code>this</code> (possibly qualified by a type),
+				</li>
+				<li>a class literal,</li>
+				<li>a literal,</li>
+				<li>a parenthesized expression,</li>
+				<li>a class instance creation expression,</li>
+				<li>a reference to a field,</li>
+				<li>an array component,</li>
+				<li>a method invocation,</li>
+				<li>an expression name,</li>
+				<li>a lambda expression.</li>
+			</ul> <span info=2> This excludes types of expressions from being the
+				operand of a reference-type&ndash;cast:
 				<ul>
-					<li>the application of any one of the following operators:
-						<ul>
-							<li>the cast operator,</li>
-							<li>the logical negation operator,</li>
-							<li>the bitwise negation operator,</li>
-							<li>the post-increment operator,</li>
-							<li>the post-decrement operator,</li>
-						</ul>
-					</li>
-					<li>any type of array creation expression,</li>
-					<li>a method reference,</li>
-					<li>the keyword <code>this</code> (possibly qualified by a type),
-					</li>
-					<li>a class literal,</li>
-					<li>a literal,</li>
-					<li>a parenthesized expression,</li>
-					<li>a class instance creation expression,</li>
-					<li>a reference to a field,</li>
-					<li>an array component,</li>
-					<li>a method invocation,</li>
-					<li>an expression name,</li>
-					<li>a lambda expression.</li>
-				</ul></span>
+					<li>the pre-increment operator,</li>
+					<li>the pre-decrement operator,</li>
+					<li>the unary plus operator, and</li>
+					<li>the unary minus operator.</li>
+				</ul>
+		</span>
 		</td>
 	</tr>
 	<tr>
@@ -131,23 +138,33 @@
 	casts always use reference types.
 </p>
 <h2>Behavior</h2>
-<p>Every expression has a type which is known at compile time. Casting
-	has runtime effects, but is usually used to change the type of an
-	expression; more specifically, a cast operator, applied to an
-	expression, is a new expression whose type is exactly that specified by
-	the cast.</p>
+<p>
+	Every expression has a type which is known at compile time<sup info=3></sup>.<span
+		info=3>Every expression is classified as either a <i>standalone</i> or
+		a <i>poly</i> expression. A standalone expression's type can be
+		determined from its contents whereas a poly expression's type can be
+		determined from its context.
+	</span> Casting has runtime effects, but is usually used to change the
+	type of an expression; using the cast operator on some expression forms
+	a new expression whose type is specified by the cast.
+</p>
 <p>A cast expression does not affect the value of its operand except in
 	the case that the operand is a lambda expression, in which case the
 	actual runtime value of the cast expression is the lambda expression of
 	type specified by the cast.</p>
-<h3>Impossible, Permissible, and Failing Casts</h3>
-<h3>Reference vs. Primitive Cast</h3>
+<h3>Casting Conversion</h3>
+<p>
+<h3>Cast Errors or Redundance</h3>
+<p>
+<h3>Reference &amp; Primitive Cast Differences</h3>
 <p>Casting to a primitive type is treated different, syntactically, from
 	casting to a reference type. To avoid syntactical ambiguity, a cast to
 	one or more reference types cannot be applied to a unary plus or minus
 	expression, or to a pre-increment or pre-decrement expression. This
 	restriction holds even in the case when an intersection cast is used,
 	despite the lack of syntactic ambiguity in with an intersection cast.</p>
+<h3>Lambdas</h3>
+<h3>Intersection Casts</h3>
 <h2>Examples</h2>
 <!-- TODO: Add examples -->
 <h2>Notes</h2>
