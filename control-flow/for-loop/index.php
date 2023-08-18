33ae7bb13f644f471758d30600c8fecacfabb519
the-scholar <the-scholar@dusttoash.org>
2023-08-17 22:51:07 -0500

    Create WIP for loop page

---
+++ control-flow/for-loop/index.php
@@ -0,0 +1,70 @@
+<?php t("Javaref - For Loops", "For loops allow re-executing a statement a number of times in Java.");?>
+<h1>For Loop</h1>
+<p class="description">A control flow construct used to repeatedly
+	execute a body statement in a structured manner.</p>
+<p>
+	For loops come in two forms: <i>Basic</i> and <i>Enhanced</i>. Basic
+	for loops contain an initializer expression, a condition, an increment
+	expression, and a body statement. One's execution begins by executing
+	its initializer statement. Then the condition is checked and if true,
+	the body, and then the increment expression are executed, in that
+	order. Enhanced for loops execute their body for every member of an <i>Iterable</i>
+	or an array type expression.
+</p>
+<h2>Syntax</h2>
+<p>For Loops can be expressed through two forms:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><code>for (</code><span class="syntax-piece optional">init-expression</span>
+			<code>;</code> <span class="syntax-piece optional">condition</span> <code>;</code>
+			<span class="syntax-piece optional">increment-expr</span> <code>)</code>
+			<span class="syntax-piece">body-statement</span></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td><code>for (</code> <span class="syntax-piece">iter-variable-decl</span>
+			<code>:</code> <span class="syntax-piece">iterable-expr</span></td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td><code>for (</code> <span class="syntax-piece">iter-variable-decl</span>
+			<code>:</code> <span class="syntax-piece">array-expr</span></td>
+	</tr>
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">syntax_piece_1</span></td>
+		<td>is syntax_piece_1_breakdown</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax piece breakdowns -->
+</table>
+<p>
+	<i>such that...</i>
+</p>
+<ul>
+	<li>
+		<!-- TODO: Insert Such that clause -->
+	</li>
+</ul>
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> syntax_fragment_1_breakdown
+</p>
+<!-- TODO: Insert any additional syntax fragment breakdowns -->
+<h2>main_section_title</h2>
+<!-- TODO: Fill out main section -->
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li>
+		<!-- TODO: Add notes -->
+	</li>
+</ol>
+<?php
+
+b();
