95c537cf8380c1b1e9ecbfd54f498e6866750f10
the-scholar <the-scholar@dusttoash.org>
2023-09-16 14:37:54 -0500

    Create expression-statements page

---
+++ statements/expression-statements/index.php
@@ -0,0 +1,45 @@
+<?php t("Javaref - Expression Statements", "Expression statements are expressions that can be executed as standalone statements.");?>
+<h1>Expression Statement</h1>
+<p class="description">concise_description</p>
+<p>
+	<!-- TODO: Full description -->
+</p>
+<h2>Syntax</h2>
+<p>Expression Statements can be expressed through:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td>syntax_fragment_1</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax fragments -->
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">syntax_piece_1</span></td>
+		<td>is syntax_piece_1_breakdown</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax piece breakdowns -->
+</table>
+<p>
+	<i>such that...</i>
+</p>
+<ul>
+	<li><!-- TODO: Insert Such that clause --></li>
+</ul>
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> syntax_fragment_1_breakdown
+</p>
+<!-- TODO: Insert any additional syntax fragment breakdowns -->
+<h2>main_section_title</h2>
+<!-- TODO: Fill out main section -->
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li><!-- TODO: Add notes --></li>
+</ol>
+<?php b();
