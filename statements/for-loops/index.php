fd7ee51c3385f795d65acef27a89d97e02796579
the-scholar <the-scholar@dusttoash.org>
2023-09-18 11:04:12 -0500

    Update for-loops info formatting

--- statements/for-loops/index.php
+++ statements/for-loops/index.php
@@ -184,10 +184,9 @@ for (Object o : dogArray);		// Dog objects can fit in the Object variable, o
 <ol id="basic-for-loop-steps">
 	<li>The <span class="syntax-piece">init-expression</span> is executed.
 	</li>
-	<li style="color: #ffe62b;">The <span class="syntax-piece">condition</span>
-		is evaluated. If it evaluates to <code>false</code>, the loop is
-		exited.
-	</li>
+	<li><span style="color: #ffe62b;">The <span class="syntax-piece">condition</span>
+			is evaluated. If it evaluates to <code>false</code>,
+	</span> the loop is exited.</li>
 	<li style="color: #ffe62b;">The <span class="syntax-piece">body-stmt</span>
 		is executed.
 	</li>
