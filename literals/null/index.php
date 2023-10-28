2efac4140054ee02f790cd12c790d684dbcce77f
the-scholar <the-scholar@dusttoash.org>
2023-10-28 02:38:33 -0500

    Update null literal page

--- literals/null/index.php
+++ literals/null/index.php
@@ -1,7 +1,8 @@
-<?php t("Javaref - Null Literal", "The null value expresses the special null value in source code, and can be assigned to any reference type.");?>
+<?php t("Javaref - Null Literals", "The null value expresses the special null value in source code, and can be assigned to any reference type.");?>
 <h1>Null Literal</h1>
 <p class="description">
-	Literal expression of the <code>null</code> value.
+	A literal (textual) expression of the <code>null</code> value in source
+	code.
 </p>
 <p>
 	The <code>null</code> literal is used to denote the special null value,
@@ -9,6 +10,38 @@
 	type is the null type. The null type cannot be expressed in Java code.
 </p>
 <h2>Syntax</h2>
-<p>The null literal has one form:</p>
+<p>The Null Literal has one form:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td><code>null</code></td>
+	</tr>
+</table>
 
-<?php b();
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> The <code>null</code> literal.
+	Parsed as a single token.
+</p>
+<h2>Usage</h2>
+<p>
+	The null value is a special type of value that's used to represent the
+	<i>lack</i> of information. The <code>null</code> literal is used to
+	explicitly write the null value in source code:
+</p>
+<pre><code>String x = null;</code></pre>
+<p>
+	The <code>null</code> value is assignable to any reference type,
+	including variables of any type parameter.
+</p>
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li>
+		<!-- TODO: Add notes -->
+	</li>
+</ol>
+<?php
+
+b();
