912a52b5eb2da83e0c37eba544597eb30d681f7c
the-scholar <the-scholar@dusttoash.org>
2023-10-29 15:01:22 -0500

    Create Unary Plus page (WIP)

---
+++ operators/unary/plus/index.php
@@ -0,0 +1,71 @@
+<?php t("Javaref - Unary Plus", "The unary plus operator converts its operand to a primitive int, long, float, or double. The value of the operand is not changed.");?>
+<h1>Unary Plus</h1>
+<p class="description">
+	Converts its operand to <code>int</code>, <code>long</code>, <code>float</code>,
+	or <code>double</code>. The value is preserved.
+</p>
+<p>
+	The unary plus operator may have any primitive numeric type<sup info=1></sup>
+	(or corresponding wrapper type). The result of the operation is the
+	same value as the input, but converted to a primitive type (if the
+	operand's type is a wrapper type) and converted to <code>int</code> (if
+	the operand is <code>byte</code>, <code>char</code>, <code>short</code>,
+	or a wrapper of one of those types).
+</p>
+<span info=1>A primitive numeric type is any primitive type other than <code>boolean</code>,
+	that is, any of the following types:
+	<ul>
+		<li><code>byte</code></li>
+		<li><code>short</code></li>
+		<li><code>char</code></li>
+		<li><code>int</code></li>
+		<li><code>float</code></li>
+		<li><code>long</code></li>
+		<li><code>double</code></li>
+	</ul>
+</span>
+<h2>Syntax</h2>
+<p>Unary Plus can be expressed through:</p>
+<table class="syntax">
+	<tr>
+		<td>1</td>
+		<td>syntax_fragment_1</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax fragments -->
+</table>
+<p>
+	<i>where...</i>
+</p>
+<table class="syntax-breakdown">
+	<tr>
+		<td><span class="syntax-piece">syntax_piece_1</span></td>
+		<td>is syntax_piece_1_breakdown</td>
+	</tr>
+	<!-- TODO: Insert any additional syntax piece breakdowns -->
+</table>
+<p>
+	<i>such that...</i>
+</p>
+<ul>
+	<li>
+		<!-- TODO: Insert Such that clause -->
+	</li>
+</ul>
+<h3>Syntax Elements</h3>
+<p>
+	<span class="syntax-number">1</span> syntax_fragment_1_breakdown
+</p>
+<!-- TODO: Insert any additional syntax fragment breakdowns -->
+<h2>main_section_title</h2>
+<!-- TODO: Fill out main section -->
+<h2>Examples</h2>
+<!-- TODO: Add examples -->
+<h2>Notes</h2>
+<ol>
+	<li>
+		<!-- TODO: Add notes -->
+	</li>
+</ol>
+<?php
+
+b();
