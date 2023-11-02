af06171db66d564933b3dcdf9832c93d7a9d0bc7
the-scholar <the-scholar@dusttoash.org>
2023-11-01 20:17:00 -0500

    Make theme use local storage

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function toggleTheme() {
-	res = document.documentElement.classList.toggle("light-theme")
-	document.getElementById(res ? "DarkThemeButton" : "LightThemeButton").style.display = 'none';
-	document.getElementById(res ? "LightThemeButton" : "DarkThemeButton").style.removeProperty('display')
-}
-
-function updateScrollbar() {
-	document.getElementById("ProgressBar").style.height = (document.documentElement.scrollTop || document.body.scrollTop) / ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight) * 100 + "%";
-}
+document.addEventListener('DOMContentLoaded', () => {
+	if (localStorage.getItem('light-theme')) {
+		res = document.documentElement.classList.toggle("light-theme")
+		document.getElementById(res ? "DarkThemeButton" : "LightThemeButton").style.display = 'none';
+		document.getElementById(res ? "LightThemeButton" : "DarkThemeButton").style.removeProperty('display')
+	}
+});
 window.addEventListener("load", (e) => {
 	for (let ref of document.querySelectorAll("sup[info]")) {
 		let element = document.querySelector("span[info='" + ref.getAttribute("info") + "']");
@@ -24,6 +22,20 @@ window.addEventListener("load", (e) => {
 		};
 	}
 });
+
+function toggleTheme() {
+	res = document.documentElement.classList.toggle("light-theme")
+	document.getElementById(res ? "DarkThemeButton" : "LightThemeButton").style.display = 'none';
+	document.getElementById(res ? "LightThemeButton" : "DarkThemeButton").style.removeProperty('display')
+	if (res)
+		localStorage.setItem("light-theme", "enabled")
+	else
+		localStorage.removeItem("light-theme")
+}
+
+function updateScrollbar() {
+	document.getElementById("ProgressBar").style.height = (document.documentElement.scrollTop || document.body.scrollTop) / ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight) * 100 + "%";
+}
 document.addEventListener("scroll", (e) => updateScrollbar());
 function copyToClipboard(text) {
 	navigator.clipboard.writeText(text).then(() => { }, () => {
