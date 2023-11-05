c0fc6dabb982220bfe1a7e5a542e3a5cfcc5c346
the-scholar <the-scholar@dusttoash.org>
2023-11-05 02:47:33 -0600

    Remove useless class from home page elements

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ ul {
 	background-color: #ffffff0a;
 	font-size: .87em;
 }
-
 </style><?php
 };
 t("Javaref.net", "The unofficial reference for Java syntax.");
@@ -24,41 +23,37 @@ t("Javaref.net", "The unofficial reference for Java syntax.");
 <h1>Java Reference</h1>
 <div class="sec">General</div>
 <ul>
-	<li><a class="c" href="/identifiers">Identifiers</a></li>
-	<li><a class="c" href="/initializers">Static &amp; Instance
-			Initializers</a></li>
+	<li><a href="/identifiers">Identifiers</a></li>
+	<li><a href="/initializers">Static &amp; Instance Initializers</a></li>
 </ul>
 <div class="sec">Concepts</div>
 <ul>
-	<li><a class="c" href="/concepts/capture-conversion">Capture Conversion</a></li>
+	<li><a href="/concepts/capture-conversion">Capture Conversion</a></li>
 </ul>
 <div class="sec">Literals</div>
 <ul>
-	<li><a class="c" href="/literals/integer">Integer Literal</a></li>
-	<li><a class="c" href="/literals/floating-point">Floating Point
-			Literals</a></li>
-	<li><a class="c" href="/literals/boolean">Boolean Literals</a></li>
-	<li><a class="c" href="/literals/null">Null Literals</a></li>
+	<li><a href="/literals/integer">Integer Literal</a></li>
+	<li><a href="/literals/floating-point">Floating Point Literals</a></li>
+	<li><a href="/literals/boolean">Boolean Literals</a></li>
+	<li><a href="/literals/null">Null Literals</a></li>
 </ul>
 <div class="sec">Statements</div>
 <ul>
-	<li><a class="c" href="/statements/for-loops">For Loops</a></li>
-	<li><a class="c" href="/statements/expression-statements">Expression
-			Statements</a></li>
+	<li><a href="/statements/for-loops">For Loops</a></li>
+	<li><a href="/statements/expression-statements">Expression Statements</a></li>
 </ul>
 <div class="sec">Methods</div>
 <ul>
-	<li><a class="c" href="/methods/declarations">Method Declarations</a></li>
-	<li><a class="c" href="/methods/receiver-parameters">Receiver
-			Parameters</a></li>
+	<li><a href="/methods/declarations">Method Declarations</a></li>
+	<li><a href="/methods/receiver-parameters">Receiver Parameters</a></li>
 </ul>
 <div class="sec">Operators</div>
 <ul>
 	<li>
 		<div class="sec">Unary</div>
 		<ul>
-			<li><a class="c" href="/operators/unary/cast">Cast Operator</a></li>
-			<li><a class="c" href="/operators/unary/minus">Unary Minus</a></li>
+			<li><a href="/operators/unary/cast">Cast Operator</a></li>
+			<li><a href="/operators/unary/minus">Unary Minus</a></li>
 			<li><a class="c u" href="/operators/unary/plus">Unary Plus</a></li>
 		</ul>
 	</li>
@@ -70,11 +65,9 @@ t("Javaref.net", "The unofficial reference for Java syntax.");
 </ul>
 <div class="sec">Annotations</div>
 <ul>
-	<li><a class="c" href="annotations">Annotations</a></li>
-	<li><a class="c" href="annotations/declarations">Annotation
-			Declarations</a></li>
-	<li><a class="c" href="annotations/elements">Annotation Element
-			Declarations</a></li>
+	<li><a href="annotations">Annotations</a></li>
+	<li><a href="annotations/declarations">Annotation Declarations</a></li>
+	<li><a href="annotations/elements">Annotation Element Declarations</a></li>
 </ul>
 <?php
 
